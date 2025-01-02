ed0735aee07a415078d619d8da903f125cba755d
raulAM7 <raulartilesm@gmail.com>
2025-01-02 09:09:12 +0000

    update

---
+++ lesson2-schema-model.js
@@ -0,0 +1,19 @@
+// Lesson 2 - Creating an Scheme and a Model
+
+
+
+/* SCHEME
+
+    Cada scheme se asigna a una Collection
+    En el scheme se definnen los campos/atributos, ditpos de datos, etc 
+
+*/
+
+
+
+
+/* MODEL
+
+    Usa el scheme para interactuar con la base de datos para realizar las operaciones del CRUD
+
+*/
